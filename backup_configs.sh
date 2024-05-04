a3fb64024fa07f3a00f8f91c7a0216a611f7e000
PaulMez <meza.leet@gmail.com>
2024-05-04 13:37:12 +1000

    backup tweak

--- backup_configs.sh
+++ backup_configs.sh
@@ -12,11 +12,15 @@ backup_dir="$HOME/.dotmez/configs"
 # Create backup directory if it doesn't exist
 mkdir -p "$backup_dir"
 
-# Loop through each file and create a backup with timestamp
+# Loop through each file, check if it exists, and create a backup with timestamp
 for file in "${files[@]}"; do
     filename=$(basename "$file")
-    timestamp=$(date +"%Y%m%d_%H%M%S")
-    backup_filename="${filename%.*}_backup_$timestamp.${filename##*.}"
-    cp "$source_dir/$filename" "$backup_dir/$backup_filename"
-    echo "Backup created: $backup_dir/$backup_filename"
+    if [ -e "$source_dir/$filename" ]; then
+        timestamp=$(date +"%Y%m%d_%H%M%S")
+        backup_filename="${filename%.*}_backup_$timestamp.${filename##*.}"
+        cp "$source_dir/$filename" "$backup_dir/$backup_filename"
+        echo "Backup created: $backup_dir/$backup_filename"
+    else
+        echo "File does not exist: $source_dir/$filename"
+    fi
 done
